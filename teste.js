eba4ca48848248db5998764a275a4546b4a72079
Gabriel Magalhães de Oliveira <99506666+MagalGabriel@users.noreply.github.com>
2022-09-14 09:31:27 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", function(req, resp){
+    resp.send("Bem-vindo ao meu app");
+})
+
+app.get("/contato", function(req, resp){
+    resp.send("Página de contato do meu app")
+})
+
+app.get("/produto", function(req, resp){
+    resp.send("Página de produto do meu app")
+})
+
+//Essa deve ser sempre a última linha quando usamos o express
 app.listen(8081, function(){
     console.log("O servidor está funcionando na url http://localhost:8081");
 });
